bcc3582cfcace4cbc335fd2f78cc254247cbb44c
Cesar Neri <ceneri@ucsc.edu>
2018-04-26 19:26:50 -0700

    Substraction and addition done 3/7 tests passed. (Messy Code)

--- P2/bigint.ml
+++ P2/bigint.ml
@@ -50,6 +50,46 @@ module Bigint = struct
         let taken, _ = fold_until (fun acc h -> h :: acc) [] (k - i + 1) list in
         List.rev taken;;
 
+    let trimzeros list =
+        let rec trimzeros' list' = match list' with
+            | []          -> []
+            | [0]         -> []
+            | car::cdr    ->
+                    let cdr' = trimzeros' cdr
+                    in match car, cdr' with
+                        | 0, []       -> []
+                        | car, cdr'   ->  car::cdr'
+        in trimzeros' list
+
+    (*Helper metyhods*)
+
+    let flip_sign sign = match sign with
+        | Pos -> Neg
+        | Neg -> Pos
+
+
+    let rec compareTo' list1 list2 = match (list1, list2) with
+        | [], []                -> 0
+        | list1, list2          -> begin
+            if car list1 > car list2 then 1
+            else if car list1 < car list2 then -1
+            else compareTo' (cdr list1) (cdr list2)
+        end
+
+
+    (**Similar to Java method returns:
+        -   1   if value1 > value2
+        - (-1)  if value1 < value2
+        -   0   if value1 = value2*)
+    let compareTo value1 value2 = 
+        let list_int1 = reverse (trimzeros value1)  
+        in let list_int2 = reverse (trimzeros value2)
+        in if List.length list_int1 > List.length list_int2
+        then 1
+        else if  List.length list_int1 < List.length list_int2
+            then -1
+            else compareTo' list_int1 list_int2;;
+
 
     let rec to_string_helper reversed_list = 
         let len = List.length reversed_list
@@ -70,17 +110,6 @@ module Bigint = struct
                        ((if sign = Pos then "" else "-") ::
                         (to_string_helper reversed))
 
-    
-
-    (*let string_of_bigint_help' value_list count =
-        let len = length value_list 
-        in if count < 70
-            then 
-                   in  let return_str = strcat ""
-                       ((if sign = Pos then "" else "-") ::
-                        (map string_of_int reversed))
-                            in add_newline return_str)*)
-
 
     let rec add' list1 list2 carry = match (list1, list2, carry) with
         | list1, [], 0       -> list1
@@ -91,12 +120,43 @@ module Bigint = struct
           let sum = car1 + car2 + carry
           in  sum mod radix :: add' cdr1 cdr2 (sum / radix)
 
+     let rec sub' list1 list2 borrow = match (list1, list2, borrow) with
+        | list1, [], false                 -> list1
+        | [], list2, false                 -> list2
+        | list1, [], true                 -> sub' list1 [1] false
+        | car1::cdr1, car2::cdr2, false  -> begin
+          if car1 > car2 then let subs = car1 - car2
+          in subs :: sub' cdr1 cdr2 false
+          else if car1 < car2 then let subs = (car1 + 10) - car2
+          in subs :: sub' cdr1 cdr2 true
+          else 0 :: sub' cdr1 cdr2 false
+        end
+        | car1::cdr1, car2::cdr2, true  ->  begin
+          if (car1 - 1) > car2 then let subs = (car1 - 1) - car2
+          in subs :: sub' cdr1 cdr2 false
+          else if (car1 - 1) < car2 then let subs = (car1 -1 + 10) - car2
+          in subs :: sub' cdr1 cdr2 true
+          else 0 :: sub' cdr1 cdr2 false
+        end 
+        | _, _, _  -> [0];;
+
+
+    let sub (Bigint (neg1, value1)) (Bigint (neg2, value2)) =
+        if neg1 <> neg2     (* Addition *)
+        then Bigint (neg1, add' value1 value2 0)
+        else let cmp = compareTo value1 value2 in
+            if cmp = 1 then Bigint (neg1, sub' value1 value2 false)
+            else if cmp = -1 then   (* Sign is flipped*)
+                Bigint (flip_sign neg2, sub' value2 value1 false)
+            else zero
+
     let add (Bigint (neg1, value1)) (Bigint (neg2, value2)) =
         if neg1 = neg2
         then Bigint (neg1, add' value1 value2 0)
-        else zero       (* Should become substarction*)
+        else sub (Bigint (neg1, value1)) (Bigint (flip_sign neg2, value2)) 
+        (* Should become substarction, sign is flipped, sub will flip it back*)
 
-    let sub = add     
+   
 
     let mul = add
 
@@ -106,15 +166,7 @@ module Bigint = struct
 
     let pow = add
 
-    (*Helper metyhods*)
-
-    (**Similar to Java method returns:
-        -   1   if value1 > value2
-        - (-1)  if value1 < value2
-        -   0   if value1 = value2
-
     
-    let compareTo value1 value2 = *)
 
 end
 
