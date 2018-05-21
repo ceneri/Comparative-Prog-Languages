9cbb5d1e4ba2e0765834118007d84b56a9415b85
Cesar Neri <ceneri@ucsc.edu>
2018-05-20 19:45:18 -0700

    P3 working, final commit. (Clean code)

---
+++ P3/differ.sh
@@ -0,0 +1,10 @@
+#Execute: sh differ.sh
+#Quickly compare output of mk.tests
+
+diff test0.zero.in test0.zero.in.unzipped;     
+diff test1.uniform.in test1.uniform.in.unzipped;
+diff test3.gif.in test3.gif.in.unzipped;
+diff test4.aaaaaaaa.in test4.aaaaaaaa.in.unzipped ;
+diff test5.ps.in test5.ps.in.unzipped;
+diff test6.huffman.in test6.huffman.in.unzipped; 
+diff test7.etaoin.in test7.etaoin.in.unzipped;
